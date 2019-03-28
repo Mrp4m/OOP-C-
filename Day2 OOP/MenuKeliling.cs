eefb8423758c78130d2104838918ba230cb7164a
Mrp4m <pmbyn.ofc@gmail.com>
2019-03-28 08:55:30 +0700

    Project "MenuKeliling" for beta

---
+++ Day2 OOP/MenuKeliling.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_OOP
+{
+    class MenuKeliling
+    {
+        static void Main(string[] args)
+        {
+            int menus;
+            decimal number1;
+            decimal number2;
+            decimal number3;
+            decimal number4;
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("-----------MENU KELILING------------");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. Segitiga");
+            Console.WriteLine("2. Lingkaran");
+            Console.WriteLine("3. Persegi Panjang");
+            Console.WriteLine("4. Trapesium");
+            Console.WriteLine(" ");
+            Console.WriteLine("------------------------------------");
+            Console.Write("Pilih Menu : ");
+            menus = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("------------------------------------");
+            
+            if (menus == 1)
+            {
+                Console.WriteLine("TRIANGLE");
+                Console.Write("Masukan SISI pertama : ");
+                number1 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI kedua   : ");
+                number2 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI ketiga  : ");
+                number3 = Convert.ToDecimal(Console.ReadLine());
+
+                Triangle p;
+                p = new Triangle(number1, number2, number3);
+                decimal c = p.proccesing();
+                Console.WriteLine("Result : " + c);
+                Console.Read();
+            }else if(menus == 2)
+            {
+                Console.WriteLine("CIRCLE");
+                Console.Write("Masukan Diameter : ");
+                number1 = Convert.ToDecimal(Console.ReadLine());
+
+                Circle p;
+                p = new Circle(number1);
+                decimal c = p.proccesing();
+                Console.WriteLine("Result : " + c);
+                Console.Read();
+            }else if(menus == 3)
+            {
+                Console.WriteLine("RECTANGLE");
+                Console.Write("Masukan SISI pertama : ");
+                number1 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI kedua   : ");
+                number2 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI ketiga  : ");
+                number3 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI keEmpat  : ");
+                number4 = Convert.ToDecimal(Console.ReadLine());
+
+                Rectangle p;
+                p = new Rectangle(number1, number2, number3, number4);
+                decimal c = p.proccesing();
+                Console.WriteLine("Result : " + c);
+                Console.Read();
+            }else if (menus == 4)
+            {
+                Console.WriteLine("TRAPESIUM");
+                Console.Write("Masukan SISI pertama : ");
+                number1 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI kedua   : ");
+                number2 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI ketiga  : ");
+                number3 = Convert.ToDecimal(Console.ReadLine());
+                Console.Write("Masukan SISI keEmpat  : ");
+                number4 = Convert.ToDecimal(Console.ReadLine());
+
+                Trapesium p;
+                p = new Trapesium(number1, number2, number3, number4);
+                decimal c = p.proccesing();
+                Console.WriteLine("Result : " + c);
+                Console.Read();
+            }else
+            {
+                Console.WriteLine("Siji tekan papat tok gan, kok koe milih selain iku");
+                Console.Read();
+            }
+        }
+        public abstract class Process
+        {
+            public abstract decimal proccesing();
+        }
+
+        public class Triangle : Process
+        {
+            public decimal sidea;
+            public decimal sideb;
+            public decimal sidec;
+            public Triangle(decimal a = 0, decimal  b = 0, decimal c = 0)
+            {
+                sidea = b;
+                sideb = a;
+                sidec = c;
+            }
+            public override decimal proccesing()
+            {
+                return (sidea + sideb + sidec);
+            }
+        }
+        public class Circle : Process
+        {
+            public decimal Pi = 22/7;
+            public decimal Diameter;
+            public Circle(decimal a = 0)
+            {
+                Diameter = a;
+            }
+            public override decimal proccesing()
+            {
+                return (Pi * Diameter);
+            }
+        }
+        public class Rectangle : Process
+        {
+            public decimal sidea;
+            public decimal sideb;
+            public decimal sidec;
+            public decimal sided;
+            public Rectangle(decimal a = 0, decimal b = 0, decimal c = 0, decimal d = 0)
+            {
+                sidea = b;
+                sideb = a;
+                sidec = c;
+                sided = d;
+            }
+            public override decimal proccesing()
+            {
+                return (sidea + sideb + sidec + sided);
+            }
+        }
+        public class Trapesium : Process
+        {
+            public decimal sidea;
+            public decimal sideb;
+            public decimal sidec;
+            public decimal sided;
+            public Trapesium(decimal a = 0, decimal b = 0, decimal c = 0, decimal d = 0)
+            {
+                sidea = b;
+                sideb = a;
+                sidec = c;
+                sided = d;
+            }
+            public override decimal proccesing()
+            {
+                return (sidea + sideb + sidec + sided);
+            }
+        }
+    }
+}
---
+++ Day2 OOP/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_OOP
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Encapsulation -> pembatasan akses agar tidak bisa diakses secara langsung----
+        //    parent bukaDompet = new parent();                                            
+        //    int dompetNya = bukaDompet.a();
+        //    //----------------PemanggilanPolymorphism--------------------
+        //    int number, number1;
+        //    Console.Write("Insert number pertama gan : ");
+        //    number = Convert.ToInt16(Console.ReadLine());
+        //    Console.Write("Insert number ke 2 gan : ");
+        //    number1 = Convert.ToInt16(Console.ReadLine());
+        //    Sub p;
+        //    p = new Sub(number,number1);
+        //    double c = p.proccesing();
+        //    Console.WriteLine("Result : " + c);
+        //    Console.Read();
+        //}   
+    }
+    public class parent
+    {
+        public int c,d;
+        private int wallet = 10000;
+        public int a()
+        {
+            return wallet;
+        }
+    }
+
+    //Inheritance -> pewaarisan sifat dari parent class
+    public class child : parent
+    {
+        public int Method1()
+        {
+            return (c * d);
+        }
+    }
+
+    //Polymorpism -> Banyak bentuk method dan bentuk implementasinya
+   
+    public abstract class Process
+    {
+        public abstract double proccesing();
+    }
+
+    public class Sub : Process
+    {
+        public double alas;
+        public double tall;
+        public Sub(double a = 0, double b = 0)
+        {
+            tall = b;
+            alas = a;
+        }
+        public override double proccesing()
+        {
+            return (0.5 * alas * tall);
+        }
+    }
+
+    //class subProcess
+    //{
+    //    static void main(string[] args)
+    //    {
+    //        Process p = new Sub(10, 7);
+    //        decimal c = p.proccesing();
+    //        Console.WriteLine("Result : " + c);
+
+    //    }
+    //}
+}
